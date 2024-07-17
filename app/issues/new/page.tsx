63552c8fefc508771e14bef27301d71673724a78
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-18 06:07:31 +0800

    Handle new bug submission errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,62 @@
 'use client'
-import { TextArea, TextField, Button } from '@radix-ui/themes'
+import React from 'react'
+import { TextArea, TextField, Button, Callout } from '@radix-ui/themes'
+
 import dynamic from 'next/dynamic' // to lazy load a heavy compo
 import 'easymde/dist/easymde.min.css'
+
 import axios from 'axios'
 import { useForm, Controller } from 'react-hook-form'
 import { useRouter } from 'next/navigation'
+
+export default function NewIssuePage() {
+  const { register, control, handleSubmit } = useForm<IssueForm>()
+  const router = useRouter()
+  const [error, setError] = React.useState('')
+
+  return (
+    <div className='max-w-xl'>
+      {error && (
+        <Callout.Root
+          color='tomato'
+          className='mb-5'
+        >
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className='space-y-3'
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post('/api/issues', data)
+            router.push('/issues')
+          } catch (error) {
+            setError('An error has occurred')
+          }
+        })}
+      >
+        <TextField.Root
+          placeholder='Title'
+          {...register('title')}
+        >
+          <TextField.Slot />
+        </TextField.Root>
+        <Controller
+          name='description'
+          control={control}
+          render={({ field }) => (
+            <SimpleMDEditor
+              placeholder='description'
+              {...field}
+            />
+          )}
+        />
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
+  )
+}
+
 const SimpleMDEditor = dynamic(() => import('react-simplemde-editor'), {
   ssr: false,
   loading: () => <p>loading...</p>,
@@ -13,37 +65,4 @@ const SimpleMDEditor = dynamic(() => import('react-simplemde-editor'), {
 interface IssueForm {
   title: string
   description: string
-}
-
-export default function NewIssuePage() {
-  const { register, control, handleSubmit } = useForm<IssueForm>()
-  const router = useRouter()
-
-  return (
-    <form
-      className='max-w-xl space-y-3'
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post('/api/issues', data)
-        router.push('/issues')
-      })}
-    >
-      <TextField.Root
-        placeholder='Title'
-        {...register('title')}
-      >
-        <TextField.Slot />
-      </TextField.Root>
-      <Controller
-        name='description'
-        control={control}
-        render={({ field }) => (
-          <SimpleMDEditor
-            placeholder='description'
-            {...field}
-          />
-        )}
-      />
-      <Button>Submit New Issue</Button>
-    </form>
-  )
 }
