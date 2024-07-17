bdbeb3a987cd4e7283e57d451fa3bde33f67304e
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-18 06:07:31 +0800

    Handle new bug submission errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,8 +3,8 @@ import { z } from 'zod'
 import prisma from '../../../prisma/client'
 
 const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  title: z.string().min(1, 'Title is required.').max(255),
+  description: z.string().min(1, 'Description is required'),
 })
 
 interface Issue {
@@ -22,7 +22,7 @@ export async function POST(req: NextRequest) {
   }
 
   if (!validation.success) {
-    return NextResponse.json(validation.error.errors, { status: 400 })
+    return NextResponse.json(validation.error.format(), { status: 400 })
   }
   const newIssue = await prisma.issue.create({ data: issue })
 
