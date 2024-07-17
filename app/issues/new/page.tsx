137db9f5272fa08fe626406813b9021410b15ad0
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-18 05:48:28 +0800

    Setup form submission for issue creation
    - Browser console displaying a warning I could overcome
    
    - Warning: Function components cannot be given refs.
     Attempts to access this ref will fail. Did you mean to use React.forwardRef()?

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,19 +2,48 @@
 import { TextArea, TextField, Button } from '@radix-ui/themes'
 import dynamic from 'next/dynamic' // to lazy load a heavy compo
 import 'easymde/dist/easymde.min.css'
+import axios from 'axios'
+import { useForm, Controller } from 'react-hook-form'
+import { useRouter } from 'next/navigation'
 const SimpleMDEditor = dynamic(() => import('react-simplemde-editor'), {
   ssr: false,
   loading: () => <p>loading...</p>,
 })
 
+interface IssueForm {
+  title: string
+  description: string
+}
+
 export default function NewIssuePage() {
+  const { register, control, handleSubmit } = useForm<IssueForm>()
+  const router = useRouter()
+
   return (
-    <div className='max-w-xl space-y-3'>
-      <TextField.Root placeholder='Title'>
+    <form
+      className='max-w-xl space-y-3'
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post('/api/issues', data)
+        router.push('/issues')
+      })}
+    >
+      <TextField.Root
+        placeholder='Title'
+        {...register('title')}
+      >
         <TextField.Slot />
       </TextField.Root>
-      <SimpleMDEditor placeholder='description' />
+      <Controller
+        name='description'
+        control={control}
+        render={({ field }) => (
+          <SimpleMDEditor
+            placeholder='description'
+            {...field}
+          />
+        )}
+      />
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   )
 }
