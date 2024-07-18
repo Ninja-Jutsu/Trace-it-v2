68eaca43a9970231f3766bbb0222918f909a4507
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-19 04:01:37 +0800

    Add the edit button
    - Use radix icons package
    - Remove the extra padding the Button compo from Radix

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,10 @@
 import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import prisma from '@/prisma/client'
-import { Card, Flex, Heading, Text } from '@radix-ui/themes'
+import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
 import ReactMarkdown from 'react-markdown'
+import { Pencil2Icon } from '@radix-ui/react-icons'
+import Link from 'next/link'
 
 interface Props {
   params: {
@@ -24,23 +26,42 @@ export default async function IssueDetailsPage({ params: { id } }: Props) {
   if (!issue) notFound()
 
   return (
-    <div>
-      <Heading
-        as='h1'
-        weight='bold'
-      >
-        {issue.title}
-      </Heading>
-      <Flex
-        gap='3'
-        my='2'
-      >
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className='prose mt-4'>
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>
-    </div>
+    <Grid
+      columns={{ initial: '1', md: '2' }}
+      gap='3'
+      width='auto'
+    >
+      <Box>
+        <Heading
+          as='h1'
+          weight='bold'
+        >
+          {issue.title}
+        </Heading>
+        <Flex
+          gap='3'
+          my='2'
+        >
+          <IssueStatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className='prose mt-4'>
+          <ReactMarkdown>{issue.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button style={{ padding: '0px' }}>
+          <Link
+            href={`/issues/${issue.id}/edit`}
+            style={linkStyles}
+          >
+            <Pencil2Icon />
+            Edit Issue
+          </Link>
+        </Button>
+      </Box>
+    </Grid>
   )
 }
+
+const linkStyles = { width: '100%', display: 'flex', alignItems: 'center', gap: '1rem', padding: '1rem' }
