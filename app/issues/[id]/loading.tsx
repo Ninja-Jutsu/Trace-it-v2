9d7933d62b48addb4f0c18df06de8ca6a50ee70e
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-19 01:40:49 +0800

    Add specific loading pages for issues

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Skeleton from 'react-loading-skeleton'
+
+export default function LoadingIssueDetailsPage() {
+  return (
+    <div>
+      <p>
+        <Skeleton />
+      </p>
+      <p>
+        <Skeleton />
+      </p>
+      <p>
+        <Skeleton />
+      </p>
+      <p>
+        <Skeleton />
+      </p>
+    </div>
+  )
+}
