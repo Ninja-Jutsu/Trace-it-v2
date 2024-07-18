afc8fead56ac73f1ab5824503e6d4f3e61ae4fd2
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-19 01:47:31 +0800

    Style the issue detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,6 +1,8 @@
 import prisma from '@/prisma/client'
 import { notFound } from 'next/navigation'
 import React from 'react'
+import { Card, Flex, Heading, Text } from '@radix-ui/themes'
+import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 
 interface Props {
   params: {
@@ -23,10 +25,17 @@ export default async function IssueDetailsPage({ params: { id } }: Props) {
 
   return (
     <div>
-      <p>{issue.title}</p>
-      <p>{issue.description}</p>
-      <p>{issue.status}</p>
-      <p>{issue.createdAt.toDateString()}</p>
+      <Heading as='h1'>{issue.title}</Heading>
+      <Flex
+        gap='3'
+        my='2'
+      >
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card>
+        <Text>{issue.description}</Text>
+      </Card>
     </div>
   )
 }
