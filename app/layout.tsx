037a896908d4cf92f92a32ecc30b29260b83448b
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-21 02:47:06 +0800

    Fix Layout code duplication

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,6 @@ import AuthProvider from './auth/AuthProvider'
 import NavBar from './NavBar'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
-const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,11 +18,6 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html
-      lang='en'
-      className={inter.className}
-    >
-      <body>
     <html
       lang='en'
       className={inter.className}
